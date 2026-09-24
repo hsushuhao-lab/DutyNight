615c9f6f76aebf034258222016ed1e1123ca275d
AsherHsu <hsushuhao127@gmail.com>
2026-09-24 13:48:24 +0800

    feat: add centralized access graph for M3-M9 routes

---
+++ prototype/src/core/AccessGraph.js
@@ -0,0 +1,13 @@
+export const ACCESS_RULES=Object.freeze({
+  FIRST_TO_SECOND_BRIDGE:{requires:['SECOND_CAMPUS_ACCESS']},
+  SECOND_TO_FIRST_BRIDGE:{requires:['SECOND_CAMPUS_ACCESS']},
+  ER_TO_HILLSIDE:{requires:['OUTDOOR_ROUTE_ACCESS']},
+  SECOND_TO_HILLSIDE:{requires:['OUTDOOR_ROUTE_ACCESS']},
+  B2_ACCESS:{requires:['SPACE_PROOF','IDENTITY_PROOF','TIME_PROOF','B2_DOOR_READY']}
+});
+
+export function canAccess(gameState,ruleId){
+  const rule=ACCESS_RULES[ruleId];
+  if(!rule)return true;
+  return rule.requires.every(flag=>gameState.getFlag(flag));
+}
